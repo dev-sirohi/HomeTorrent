8cdc030fe5d49f91649b32c4ff6a7902619a204c
Dev Sirohi <devsirohi000@gmail.com>
2024-09-15 02:19:44 +0530

    started working on BEncoding decoding

--- HomeTorrent/BEncoding.cs
+++ HomeTorrent/BEncoding.cs
@@ -8,6 +8,44 @@ namespace HomeTorrent
 {
     public static class BEncoding
     {
+        // We are storing the byte values of each of these characters for comparison later.
+        // GetBytes() converts any char/string given to it into a byte array.
+        // Here, since we're only dealing with single letter strings, there is only one value in byte array.
+        // If there were more, we'd have more byte numbers.
+        private static byte DictionaryStart = System.Text.Encoding.UTF8.GetBytes("d")[0];
+        private static byte DictionaryEnd = System.Text.Encoding.UTF8.GetBytes("e")[0];
+        private static byte ListStart = System.Text.Encoding.UTF8.GetBytes("l")[0];
+        private static byte ListEnd = System.Text.Encoding.UTF8.GetBytes("e")[0];
+        private static byte NumberStart = System.Text.Encoding.UTF8.GetBytes("i")[0];
+        private static byte NumberEnd = System.Text.Encoding.UTF8.GetBytes("e")[0];
+        private static byte ByteArrayDivider = System.Text.Encoding.UTF8.GetBytes(":")[0];
 
+        public static object Decode(byte[] bytes)
+        {
+            IEnumerator<byte> enumerator = ((IEnumerable<byte>)bytes).GetEnumerator();
+            enumerator.MoveNext(); // enumerator starts before the first element
+
+            return DecodeNextObject(enumerator);
+        }
+
+        private static object DecodeNextObject(IEnumerator<byte> enumerator)
+        {
+            if (enumerator.Current == DictionaryStart)
+            {
+                return DecodeDictionary(enumerator);
+            }
+
+            if (enumerator.Current == ListStart)
+            {
+                return DecodeList(enumerator);
+            }
+
+            if (enumerator.Current == NumberStart)
+            {
+                return DecodeNumber(enumerator);
+            }
+
+            return DecodeByteArray(enumerator);
+        }
     }
 }
--- HomeTorrent/obj/Debug/net8.0/HomeTorrent.AssemblyInfo.cs
+++ HomeTorrent/obj/Debug/net8.0/HomeTorrent.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeTorrent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fa1123659eb98aeddbca1b5d3019338c510291a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeTorrent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeTorrent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
